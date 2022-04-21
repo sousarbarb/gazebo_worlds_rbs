4374b1e682c6ad3ab8a033adf1e4824dcb2726bd
Ricardo B. Sousa <sousa.ricardob@outlook.com>
2022-04-21 09:50:07 +0100

    Compute outer dimensions of the FEUP-DEEC

--- src/feup/main_simplified.m
+++ src/feup/main_simplified.m
@@ -23,3 +23,10 @@ plot([0 max(X)],[0 A*max(X)],'b-')
 grid on
 xlabel('X , Y (px) \rightarrow')
 ylabel('X , Y (mm) \rightarrow')
+
+%% DEPARTMENT SIZE
+img2 = imread("../../doc/feup/feup-deec_floor(-1)_cropped.png");
+size_department_px = size(img2);
+size_department_mm = A*size_department_px;
+size_department_m  = size_department_mm/1000;
+fliplr(size_department_m(1:2))
